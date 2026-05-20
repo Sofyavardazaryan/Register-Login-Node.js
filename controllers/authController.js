1d240727f6754e11c3052fd031299f8dd317666d
Sofya Vardazaryan <vardazaryansofia123@gmail.com>
2026-05-20 12:20:05 +0400

    4th commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,21 +12,16 @@ class AuthController {
 
   async register(req, res) {
     const { name, age, email, password } = req.body;
-
     if (!name || !age || !email || !password) {
       return res.send("All fields required");
     }
-
     const users = getUsers();
-
     const emailExists = users.find((user) => user.email === email);
-
     if (emailExists) {
       return res.send("Email already exists");
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-
     const newUser = {
       id: Date.now(),
       name,
@@ -36,31 +31,22 @@ class AuthController {
     };
 
     users.push(newUser);
-
     saveUsers(users);
-
     res.redirect("/login");
   }
 
   async login(req, res) {
     const { email, password } = req.body;
-
     const users = getUsers();
-
     const user = users.find((user) => user.email === email);
-
     if (!user) {
       return res.send("User not found");
     }
-
     const isMatch = await bcrypt.compare(password, user.password);
-
     if (!isMatch) {
       return res.send("Wrong password");
     }
-
     req.session.user = user;
-
     res.redirect("/profile");
   }
 }
